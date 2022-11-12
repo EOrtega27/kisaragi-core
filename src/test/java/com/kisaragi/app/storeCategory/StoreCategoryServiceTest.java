7656ead10b7bf77c3c8c4d14e6e811af1a3d0a57
EOrtega27 <ortegachaveznsm@gmail.com>
2022-11-11 21:00:47 -0500

    Store Category Tests

---
+++ src/test/java/com/kisaragi/app/storeCategory/StoreCategoryServiceTest.java
@@ -0,0 +1,12 @@
+package com.kisaragi.app.storeCategory;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreCategoryServiceTest {
+
+    @Test
+    void saveCategory() {
+    }
+}
