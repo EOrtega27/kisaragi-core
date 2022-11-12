e3f6cd80dccbe1434e0e6cbf78ad912a9f4a6d08
EOrtega27 <ortegachaveznsm@gmail.com>
2022-11-12 04:40:33 -0500

    CrossOrigin

--- src/main/java/com/kisaragi/app/KisaragiCoreApplication.java
+++ src/main/java/com/kisaragi/app/KisaragiCoreApplication.java
@@ -17,7 +17,10 @@ public class KisaragiCoreApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**");
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*");
 			}
 		};
 	}
