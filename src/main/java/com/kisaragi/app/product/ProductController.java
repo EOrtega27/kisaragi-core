2fe7c99a0c76d083325cb0bd2c27373d9ba4c8e8
EOrtega27 <ortegachaveznsm@gmail.com>
2022-12-02 23:07:48 -0500

    Product Controller

--- src/main/java/com/kisaragi/app/product/ProductController.java
+++ src/main/java/com/kisaragi/app/product/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/product_categories")
+@RequestMapping("/products")
 public class ProductController {
     @Autowired
     ProductService productService;
