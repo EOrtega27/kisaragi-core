c3274ae26c1dfa8add4557a14bd861e164218cf0
EOrtega27 <ortegachaveznsm@gmail.com>
2022-12-23 18:59:23 -0500

    README.md

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO store(
-	id, address, admin_id, email, latitude, longitude, name, telephone)
-	VALUES (1, "Av. Argentina 123", "usertest123456", "panadería@estacion.com", 123.34f, 123.34f, "Panaderia Estación", "984233112");
